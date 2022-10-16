b99cd7fea45b738f559f7300b85e08b01fd06e2b
gtkim <rlarbxo157@naver.com>
2022-10-16 15:05:13 +0900

    sd

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,9 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BoardService {
+  constructor(@InjectRepository(Board) private boardRepository: Repository<Board>){}
+  
   create(createBoardDto: CreateBoardDto) {
     return 'This action adds a new board';
   }
